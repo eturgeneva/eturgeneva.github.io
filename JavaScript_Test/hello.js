8831958f97d4c39ae358edbbc61c6c2a5858184f
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-10-30 12:33:29 +0100

    JS course materials

--- JavaScript_Test/hello.js
+++ JavaScript_Test/hello.js
@@ -22,3 +22,31 @@ console.log(secretMessage);
 console.log(secretMessage.length);
 
 console.log(secretMessage.join(' '));
+
+const input = 'Evgeny is a bear u';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = []; // Stores the result from the input string
+for (let i = 0; i < input.length; i++) {
+//  console.log(input[i]);
+//  console.log(i);
+
+
+  if (input[i].toLowerCase() === 'e') {
+    resultArray.push(input[i]);
+  }
+  if (input[i] === 'u') {
+    resultArray.push(input[i]);
+  }
+
+  for (let j = 0; j < vowels.length; j++) {
+    // console.log(j);
+    if (input[i].toLowerCase() === vowels[j]) {
+      // console.log(input[i], vowels[j]);
+      resultArray.push(input[i]);
+    }
+  }
+}
+ console.log(resultArray);
+ let resultString = resultArray.join('').toUpperCase();
+ 
+ console.log(resultString);
