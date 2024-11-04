c24a6e390a8cef2032348a777d85b44cc05b1d6d
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-04 11:44:23 +0100

    JS elves finished

--- JavaScript_Test/elves.js
+++ JavaScript_Test/elves.js
@@ -1,4 +1,7 @@
+// https://adventofcode.com/2023/day/1
+
 const textArray = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
+let sumOfDigits = 0;
 
 for (let i = 0; i < textArray.length; i++) {
     const textString = textArray[i];
@@ -7,12 +10,7 @@ for (let i = 0; i < textArray.length; i++) {
     console.log(textString);
 
     for (let j = 0; j < textString.length; j++) {
-
-        // console.log(textString[j]);
-        // console.log(typeof textString[j]);
-        
         if (!isNaN(textString[j])) {
-            // console.log(textString[j]);
             firstDigit = textString[j];
             break;
         }
@@ -20,11 +18,18 @@ for (let i = 0; i < textArray.length; i++) {
     console.log(firstDigit);
 
     for (let j = textString.length - 1; j >= 0; j--) {
-        // console.log(textString[j]);
         if (!isNaN(textString[j])) {
             lastDigit = textString[j];
             break;
         }
     }
     console.log(lastDigit);
+    console.log(firstDigit + lastDigit); //bc the type is a string
+    let comboOfDigits = Number(firstDigit + lastDigit);
+    console.log(comboOfDigits);
+    sumOfDigits = sumOfDigits + comboOfDigits; 
+    console.log(sumOfDigits);   
 }
+console.log(sumOfDigits); 
+
+
