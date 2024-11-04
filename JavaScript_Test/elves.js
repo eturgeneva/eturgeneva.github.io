319da1311c7e448edf7789fb0c2801b4889b1ca7
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-04 11:22:43 +0100

    JS elves last digit found

--- JavaScript_Test/elves.js
+++ JavaScript_Test/elves.js
@@ -1,8 +1,9 @@
 const textArray = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet'];
 
-// let firstDigit;
 for (let i = 0; i < textArray.length; i++) {
     const textString = textArray[i];
+    let firstDigit;
+    let lastDigit;
     console.log(textString);
 
     for (let j = 0; j < textString.length; j++) {
@@ -11,8 +12,19 @@ for (let i = 0; i < textArray.length; i++) {
         // console.log(typeof textString[j]);
         
         if (!isNaN(textString[j])) {
-            console.log(textString[j]);
+            // console.log(textString[j]);
+            firstDigit = textString[j];
             break;
         }
     }
+    console.log(firstDigit);
+
+    for (let j = textString.length - 1; j >= 0; j--) {
+        // console.log(textString[j]);
+        if (!isNaN(textString[j])) {
+            lastDigit = textString[j];
+            break;
+        }
+    }
+    console.log(lastDigit);
 }
