e517288c3dec18ce0656b311173c59cd2120013c
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-14 16:12:18 +0100

    JS organism common percentages excluding same species

--- JavaScript_Test/organism.js
+++ JavaScript_Test/organism.js
@@ -34,11 +34,8 @@ function pAequorFactory (number, dnaArray) {
           }
         // console.log(commonDna);
         let percentageRounded = (commonDna.length / this.dna.length * 100).toFixed();
-        if (commonDna.length === this.dna.length) {
-          console.log('same specie')
-        }
-        // return commonDna;
-        console.log(`specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percentageRounded}% DNA in common`)
+        return percentageRounded;
+        // console.log(`specimen #${this.specimenNum} and specimen #${pAequor.specimenNum} have ${percentageRounded}% DNA in common`)
     },
     willLikelySurvive() {
         let survivalBase = this.dna.filter(element => element === 'C' || element === 'G')
@@ -115,11 +112,12 @@ function twoRelatedSpecies (speciesArray) {
   let dnaChains = [];
   for (let i = 0; i < speciesArray.length; i++) {
     for (let j = 0; j < speciesArray.length; j++) {
-      console.log(speciesArray[i].compareDNA(speciesArray[j]));
-      // let chain = speciesArray[i].compareDNA(speciesArray[j]);
-      // console.log(chain);
+      if (i !== j) {
+        console.log(speciesArray[i].compareDNA(speciesArray[j]));
+      }
+
     }
-  // console.log(dnaChains);
+  console.log('---');
   }
 }
 twoRelatedSpecies(pAequorThatCanSurvive);
