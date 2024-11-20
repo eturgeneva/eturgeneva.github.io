01097638b61e7b0f566c1ac81092ad64b541d0f9
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-20 10:45:05 +0100

    JS phrase generator advanced wip

--- JS_phrase_generator/JS_phrase_generator_advanced.js
+++ JS_phrase_generator/JS_phrase_generator_advanced.js
@@ -1,18 +1,22 @@
 const phraseStart = ['You', 'Believe'];
+const youPart2 = ['are', 'are doing'];
+const youPart3 = ['amazing', 'awesome', 'great', 'fantastic'];
 
-const phrasePart2 = ['are', 'are doing'];
+const believePart2 = ['in', 'that'];
+const believePart3 = [
+                        ['yourself, for you are stronger than you think', 'your dreams, and they will guide you to greatness', 'the beauty of your unique journey'],
+                        ['every challenge is an opportunity in disguise', 'today is the perfect day to begin anew', 'every small step takes you closer to your goals', 'the best is yet to come']
+                     ];
 
-const phrasePart3 = ['amazing', 'awesome', 'great', 'fantastic'];
 
 function generatePhrase () {
     const beginning = phraseStart[Math.floor(Math.random() * phraseStart.length)];
+    if (beginning === 'You') {
+        const string2 = youPart2[Math.floor(Math.random() * youPart2.length)];
+        const string3 = youPart3[Math.floor(Math.random() * youPart3.length)];
 
-
-
-    const string2 = phrasePart2[Math.floor(Math.random() * phrasePart2.length)];
-    const string3 = phrasePart3[Math.floor(Math.random() * phrasePart3.length)];
-
-    return `${beginning} ${string2} ${string3}!`
+        return `${beginning} ${string2} ${string3}!`
+    }
 }
 
 // console.log(generatePhrase('elena', 'loves', 'evgeny'));
