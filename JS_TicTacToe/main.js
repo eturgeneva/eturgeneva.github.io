8ce2b6d285b3ec3b8f3b9373e4581b7789c889d8
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-23 13:50:43 +0100

    Add winning indices and more variables to JS_TicTacToe

--- JS_TicTacToe/main.js
+++ JS_TicTacToe/main.js
@@ -1,4 +1,27 @@
 let a;
 let b;
+let humanChoice;
+
+// get a children array through getElementsByClassName or getElementsByTagName
+// winning combos (children array indices):
+const winngingIndices = [
+                    [0, 1, 2],
+                    [3, 4, 5],
+                    [6, 7, 8],
+
+                    [0, 3, 6],
+                    [1, 4, 7],
+                    [2, 5, 8],
+
+                    [0, 4, 8],
+                    [2, 4, 6]
+];
+// computer makes a choice every _even_ something and _if_ there are still unfilled slots
+// computer starts from a corner if possible
+// game stops when all slots are filled
+
+// push choice into these arrays and compare the chunks with winning arrays:
+let humanArray = [];
+let computerArray = [];
 
 document.getElementById('cell1').innerHTML = 0;
