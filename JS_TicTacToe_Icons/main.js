6ae09f4b2bab07ae9f7e669e687b15824af90d5f
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-11-27 09:44:55 +0100

    Add computerArray push method to the computerChoice function for JS_TicTacToe_Icons

--- JS_TicTacToe_Icons/main.js
+++ JS_TicTacToe_Icons/main.js
@@ -24,31 +24,8 @@ const winngingIndices = [
 let humanArray = [];
 let computerArray = [];
 
-// document.getElementById('cell1').innerHTML = 0;
-
-// add if empty:
-// if (document.getElementById('cell1').innerHTML === '') {
-//     document.getElementById('cell2').innerHTML = 0;
-// }
-
 let cellsArray = document.getElementsByClassName('cell');
 console.log(cellsArray);
-
-// function revealSymbol () {
-//     document.getElementById('cell1').innerHTML = 'X';
-//     document.getElementById('cell1').style.opacity = '1';
-// }
-// document.getElementById('cell1').addEventListener('click', revealSymbol);
-
-// function revealSymbol1 () {
-//     cellsArray[0].style.opacity = '1';
-// }
-// function revealSymbol2 () {
-//     cellsArray[1].style.opacity = '1';
-// }
-// function revealSymbol3 () {
-//     cellsArray[2].style.opacity = '1';
-// }
 
 // document.getElementById('cell1').addEventListener('click', revealSymbol1);
 // document.getElementById('cell2').addEventListener('click', revealSymbol2);
@@ -56,9 +33,6 @@ console.log(cellsArray);
 // function delay(ms) {
 //     return new Promise(resolve => setTimeout(resolve, ms));
 // }
-
-// let cross = document.getElementById('cell2');
-// let circle = document.getElementById('circle');
 
 let circle = '<span class="material-symbols-outlined circle" id="circle">circle</span>';
 let cross = '<span class="material-symbols-outlined cross">close</span>'
@@ -80,33 +54,17 @@ function humanClick() {
 humanClick();
 
 function computerChoice () {
-    // if (humanArray.length >= 1) {
+  
         let i = Math.floor(Math.random() * cellsArray.length);
         console.log(i);
-        // if (cellsArray[i].innerHTML = 'X') {
+       
         while ((cellsArray[i].innerHTML === cross || cellsArray[i].innerHTML === circle) && humanArray.length < 5) {
             i = Math.floor(Math.random() * cellsArray.length);
             console.log(i);
         }
         cellsArray[i].innerHTML = circle;
         cellsArray[i].style.opacity = '1';
-    // }
+        computerArray.push(i);
+        console.log(`Computer Array: ${computerArray}`);
+
 }
-
-// function computerChoice () {
-//     if (humanArray.length >= 1) {
-//         let i = Math.floor(Math.random() * cellsArray.length);
-//         if (cellsArray[i].style.opacity = '0') {
-//             cellsArray[i].innerHTML = 'O';
-//             cellsArray[i].style.opacity = '1';
-//         }
-//     }
-// }
-
-// reveals the rest starting from cell4 in one click:
-// function revealTheRest () {
-//     for (let i = 3; i < cellsArray.length; i++) {
-//         cellsArray[i].style.opacity = '1';
-//     }
-// }
-// cellsArray[3].addEventListener('click', revealTheRest);
