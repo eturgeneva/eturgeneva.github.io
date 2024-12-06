5d647ef93eb65251387fa21de0e7ee39ddce14e9
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-12-06 14:48:30 +0100

    Update JS_Lodash practice

--- JS_Lodash/main.js
+++ JS_Lodash/main.js
@@ -75,29 +75,66 @@ const _ = {
         }
         return invertedObject;
 
-    }
+    },
+
+    findKey (object, predicate) {
+        for (let key in object) {
+            if (predicate(object[key])) {
+              return key;
+            }
+          }
+        return undefined;
+    },
+
+    drop (array, n) {
+        if (n === undefined) {
+          n = 1;
+        }
+        let droppedArray = array.slice(0 + n, array.length)
+        return droppedArray;
+      }
+    
 
 };
-    const evgeny = {
-    name: 'Evgeny',
-    job: 'developer'
-    }
 
-    console.log(_.invert(evgeny));
+const arr1 = [1, 2, 3, 4, 5]
 
+console.log(_.drop(arr1, 3))
+console.log(_.drop(arr1))
+// _.drop(arr1, 3) // must print [4, 5]
 
-  // console.log(_.clamp(5, 2, 7));
-  // console.log(_.clamp(10, -10, 7));
-  
-  // console.log(_.inRange(5, 2, 10));
-  // console.log(_.inRange(5, 2));
-  // console.log(_.inRange(5, 10, 3));
-  
-  //   console.log(_.words('elena, loves evgeny!'));
-  
-  //   console.log(_.pad('elena', 10));
-  //   _.pad('elena', 10);
+//Tests:
+// console.log(_.clamp(5, 2, 7));
+// console.log(_.clamp(10, -10, 7));
 
+// console.log(_.inRange(5, 2, 10));
+// console.log(_.inRange(5, 2));
+// console.log(_.inRange(5, 10, 3));
+
+//   console.log(_.words('elena, loves evgeny!'));
+
+//   console.log(_.pad('elena', 10));
+//   _.pad('elena', 10);
 
 //   console.log(_.has({name: 'Evgeny', job: 'developer'}, 'name'))
 //   console.log(_.has(evgeny, 'name'))
+
+
+// Object methods tests:
+
+// const evgeny = {
+//     name: 'Evgeny',
+//     job: 'developer',
+//     flaws: 0
+// }
+// console.log(_.invert(evgeny));
+
+// console.log(_.findKey(evgeny, function(elem) {
+//     return elem === 'developer';
+// }  ));
+
+// console.log(_.findKey(evgeny, function(elem) {
+//     return elem === 0;
+// }  ));
+
+// Array tests:
