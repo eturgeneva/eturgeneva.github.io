60fd3112fc3532cc395f27000034ea4a108de46f
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-12-13 17:24:38 +0100

    Add ESM module with and without destructuring

--- JS_modules_test_ESM/main.mjs
+++ JS_modules_test_ESM/main.mjs
@@ -1,5 +1,10 @@
 const number1 = 10;
 const number2 = 5;
 
+// Destructuring:
 import { sumOfNumbers } from "./module.mjs";
 console.log(sumOfNumbers(number1, number2));
+
+// Without destructuring:
+// import * as calculatorFunctions from "./module.mjs";
+// console.log(calculatorFunctions.sumOfNumbers(number1, number2));
