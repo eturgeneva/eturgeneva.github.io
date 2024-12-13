c8d61a70b1d049699a4198a5773b9f3ba725e010
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-12-13 10:06:06 +0100

    Add primary class in classes_school.js

--- JavaScript_Test/classes_school.js
+++ JavaScript_Test/classes_school.js
@@ -2,7 +2,7 @@ class School {
     constructor(name, level, numberOfStudents) {
       this._name = name;
       this._level = level;
-      this._numberOfStundents = numberOfStudents;
+      this._numberOfStudents = numberOfStudents;
     }
     get name() {
       return this._name;
@@ -15,23 +15,34 @@ class School {
     }
     set numberOfStudents(num) {
       if (typeof num === 'number') {
-        this._numberOfStundents = num;
+        this._numberOfStudents = num;
       } else {
         console.log('The value has to be a number')
       }
     }
     quickFacts() {
-  
     }
     static pickSubstituteTeacher() {
-  
     }
-  
   }
   
-  const schoolTest = new School ('Evgeny', '35', 5)
+  class Primary extends School {
+    constructor(name, level, numberOfStudents, pickupPolicy) {
+      super(name, level, numberOfStudents);
+      this._pickupPolicy = pickupPolicy;
+    }
+    get pickupPolicy() {
+      return this._pickupPolicy;
+    }
+  }
+  const primaryTest = new Primary('Elenas Primary School', 'primary', 250, 'school bus')
+  console.log(primaryTest);
+  console.log(primaryTest.numberOfStudents)
+  console.log(primaryTest.pickupPolicy)
+  
+  const schoolTest = new School ('Evgenys School', 'ultimate', 500)
   console.log(schoolTest);
-  schoolTest.numberOfStudents = 6;
+  schoolTest.numberOfStudents = 650;
   console.log(schoolTest);
   schoolTest.numberOfStudents = '7';
   console.log(schoolTest);
