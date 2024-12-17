523fec84e2177a6d97508f41e3a8899e3eaaf99c
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2024-12-17 13:42:13 +0100

    Add formatNumber function to utilities.js for JS_Modules_Practice

---
+++ JS_Modules_Practice/modules/utilities.js
@@ -0,0 +1,20 @@
+function formatNumber (number) {
+    let numberAsString = number.toString();
+    // let slices = [];
+    let step = 3;
+    for (let i = numberAsString.length - step; i > 0; i -= step) {
+        numberAsString = numberAsString.slice(0, i) + ',' + numberAsString.slice(i);
+        console.log(i);
+        console.log(numberAsString);
+    }
+    // for (let i = 0; i < numberAsString.length; i += step) {
+
+    //     slices.push(numberAsString.slice(i, i + step));
+    //     console.log(i);
+    // }
+    // return slices.join();
+    return numberAsString;
+}
+  
+console.log(formatNumber(355));
+console.log(formatNumber(355278540));
