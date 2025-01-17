936ad027b17e76958da88a9153eb87584db7c6cf
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-01-17 11:31:19 +0100

    JS async await examples

--- JS_async_await_Beans/app.js
+++ JS_async_await_Beans/app.js
@@ -1,4 +1,5 @@
 const shopForBeans = require('./library.js');
+const cookBeanSouffle = require('./library.js');
 
 async function getBeans() {
   console.log(`1. Heading to the store to buy beans...`);
@@ -8,3 +9,15 @@ async function getBeans() {
 
 getBeans();
 
+// Write your code below:
+async function hostDinnerParty() {
+  try {
+    let dinner = await cookBeanSouffle()
+    console.log(`${dinner} is served!`)
+  } catch (error) {
+    console.log(error);
+    console.log('Ordering a pizza!')
+  }
+}
+
+hostDinnerParty()
