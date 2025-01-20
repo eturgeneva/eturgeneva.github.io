0c182d54d72bbffe7a3580fac54f53f20c3feaaf
eturgeneva <91604442+eturgeneva@users.noreply.github.com>
2025-01-20 19:20:46 +0100

    JS weather API practice

--- JS_API_my_practice/app.js
+++ JS_API_my_practice/app.js
@@ -1,7 +1,11 @@
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=a526289c4ac0e48a799eabdf5fd3eaba';
 
-function populate() {
-    document.getElementById('field').innerHTML = '<p>Test</p>'
+// function populate() {
+//     document.getElementById('field').innerHTML = '<p>Test</p>'
+// }
+
+function populate(arg) {
+    document.getElementById('field').innerHTML = `<p>${arg}</p>`
 }
 
 // const request = new Request(url);
@@ -19,6 +23,14 @@ fetch(url).then(response => {
 
     // works:
     console.log(jsonResponse)
-    // return jsonResponse;
+
+    const jsString = JSON.stringify(jsonResponse);
+    console.log(jsString)
+    const jsObject = JSON.parse(jsString);
+    console.log(jsObject);
+    console.log(jsObject.coord);
+
+    // document.getElementById('field').innerHTML = `<p>${jsObject.coord.lon}</p>`
+    populate(`lon: ${jsObject.coord.lon} lat: ${jsObject.coord.lat}`)
 })
-populate();
+// populate('Test2');
